54dd31427b6597f8b93474449cc7cc80b7954fd1
Peyutron <41840787+Peyutron@users.noreply.github.com>
2024-11-01 23:13:44 +0100

    Add files via upload

---
+++ Sockets/gps_adxl_socket_server/ESP8266_adxl_6mv2_AP_server/adxl_6mv2_AP_server.ino
@@ -0,0 +1,153 @@
+/*
+ * Programa asociado a gps adxl socket server:
+ * https://github.com/Peyutron/python-Warehouse/tree/main/Sockets/gps_adxl_socket_server
+ * 
+ * Microcontrolador: ESP8266
+ * Modo AP:
+ * - SSID: gpsServer
+ * - PASS: gps12345
+ *
+ * Sensores:
+ * - ADXL345: https://github.com/adafruit/Adafruit_ADXL345
+ * - NEO6MV2: https://github.com/mikalhart/TinyGPSPlus
+ *
+ * Software desarollado por Carlos Muñoz para https://www.infotronikblog.com
+ * Fecha: 01/11/2024
+ *
+ */
+ 
+ #include "ESP8266WiFi.h"
+
+// Librerías para sensor ADXL_345
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+
+// Librerías para GPS6MV2
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+
+const char* ssid = "gpsServer";
+const char* password =  "gps12345";
+                         
+WiFiServer wifiServer(1314);
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+/*
+ * This sample code demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
+ * It requires the use of SoftwareSerial, and assumes that you have a
+ * 4800-baud serial GPS device hooked up on pins (rx) and 3(tx).
+ */
+static const int RXPin = 0, TXPin = 2;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+boolean GPSWiringError = false;
+String gpsdate = "";
+String gpstime = "";
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+unsigned long previousMillisError = 0;
+const long intervalErrorGPS = 5000;
+
+
+// Inicia el Serial, GPS NEO 6M V2 y comunicación como server
+
+void setup() {
+	Serial.begin(115200);
+	
+  delay(10);
+
+	ss.begin(GPSBaud);
+ 
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  
+  
+  /* Initialise the sensor ADXL345 */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  accel.setRange(ADXL345_RANGE_16_G);
+
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Display additional settings (outside the scope of sensor_t) */
+  displayDataRate();
+  displayRange();
+  Serial.println("");
+
+    wifiServer.begin();
+
+}
+
+void loop() {
+
+
+ WiFiClient client = wifiServer.available();
+ 
+  if (client) {
+    Serial.println("Client connected");
+    while (client.connected())
+    {
+      client.print(getADXL());
+
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= interval) 
+      {
+        previousMillis = currentMillis;
+        if(!GPSWiringError) client.print(neo6mv2data());
+        Serial.println(neo6mv2data());
+      }
+      
+      if ((currentMillis - previousMillisError >= intervalErrorGPS) && (gps.charsProcessed() < 10))
+      {
+        previousMillisError = currentMillis;
+        GPSWiringError = true;
+		    Serial.println(F("No GPS data received: check wiring"));
+        client.print("<gps check wiring>");
+      } else {
+        if (gps.charsProcessed() > 10) GPSWiringError = false;
+      }
+
+      smartDelay(25);
+
+  } // end client.connected
+  client.stop();
+  Serial.println("Client disconnected");
+ 
+  } // end Client
+}
+
+// This custom version of delay() ensures that the gps object
+// is being "fed".
+static void smartDelay(unsigned long ms) {
+	unsigned long start = millis();
+	do {
+		while (ss.available()) {
+			gps.encode(ss.read());
+		}
+	} while (millis() - start < ms);
+}
---
+++ Sockets/gps_adxl_socket_server/ESP8266_adxl_6mv2_AP_server/neo6mv2.ino
@@ -0,0 +1,58 @@
+
+// Función que retorna un String con todo los datos 
+// que recibe el módulo GPS-6MV2 en la comunicación serial
+String neo6mv2data() {
+  int satellites = 0;
+  float hdop = 0.0;
+  float location_lat = 0.0;
+  float location_lng = 0.0;
+  int age = 0;
+  char dateBuffer[10]; // 00/00/0000
+  char timeBuffer[8]; // 00:00:00
+  float altitude = 0.0;
+  float course = 0.0;
+  float speed = 0.0;
+
+  if (gps.satellites.isValid()) satellites = gps.satellites.value();
+  if (gps.hdop.isValid()) hdop = gps.hdop.hdop();
+  if (gps.location.isValid()) location_lat = gps.location.lat();
+  if (gps.location.isValid()) location_lng = gps.location.lng();
+  if (gps.location.isValid()) age = gps.location.age();
+  if (gps.altitude.isValid()) altitude = gps.altitude.meters();
+  if (gps.course.isValid()) course = gps.course.deg();
+  if (gps.speed.isValid()) speed = gps.speed.kmph();
+
+  if (gps.date.isUpdated())
+    {
+    sprintf(dateBuffer, "%d/%d/%d", gps.date.day(), gps.date.month(), gps.date.year());
+    gpsdate = String(dateBuffer);
+    }
+
+  if (gps.time.isUpdated())
+    {
+    sprintf(timeBuffer, "%d:%d:%d", gps.time.hour(), gps.time.minute(), gps.time.second());
+    gpstime = String(timeBuffer);
+    }
+
+  return "<gps" 
+            + String(satellites)
+            + " " 
+            + String(hdop)
+            + " " 
+            + String(location_lat,6)
+            + " "
+            + String(location_lng,6)
+            + " "
+            + String(age)
+            + " "
+            + String(gpsdate)
+            + " "
+            + String(gpstime)
+            + " "
+            + String(altitude, 2)
+            + " "
+            + String(course, 2)
+            + " "
+            + String(speed, 2)
+            + ">";
+}
